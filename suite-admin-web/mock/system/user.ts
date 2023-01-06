faf71005a5f09d347aa9d4e2888b0e206a5ef672
liyc <38707145@qq.com>
2023-01-06 21:14:59 +0800

    mock

---
+++ suite-admin-web/mock/system/user.ts
@@ -0,0 +1,30 @@
+export default [
+    {
+      url: '/login',
+      timeout: 1000,
+      method: 'post',
+      response: () => {
+        return {
+            code: 200,
+            result: {
+                "access_token": "0a7aaca279f6464194099a4124930b38",
+                "expired_in": 86400,
+                "refresh_token": "973e53ef385b41f8bc77d7977e3e0516"
+            },
+            message: 'ok',
+            type: 'success',
+          };
+      },
+    },
+    // {
+    //   url: '/api/admin_info',
+    //   timeout: 1000,
+    //   method: 'get',
+    //   response: () => {
+    //     // const token = getRequestToken(request);
+    //     // if (!token) return resultError('Invalid token');
+    //     return resultSuccess(adminInfo);
+    //   },
+    // },
+  ];
+  
--- suite-admin-web/vite.config.ts
+++ suite-admin-web/vite.config.ts
@@ -1,46 +1,74 @@
-import { defineConfig } from 'vite'
+import type { UserConfig, ConfigEnv } from 'vite';
 import vue from '@vitejs/plugin-vue'
 import { resolve } from 'path';
+import { viteMockServe } from 'vite-plugin-mock';
 
 const pathResolve = (dir) => {
   return resolve(__dirname, '.', dir);
 };
 
+// const configMockPlugin = (command) => {
+//   return viteMockServe({
+//     ignore: /^\_/,
+//     mockPath: 'mock',
+//     localEnabled: command === 'serve',
+//     prodEnabled: command !== 'serve',
+//     injectCode: `
+//        import { setupProdMockServer } from './mock/_createProductionServer';
+ 
+//        setupProdMockServer();
+//        `,
+//   });
+// }
+
 // https://vitejs.dev/config/
-export default defineConfig({
-  resolve: {
-    // 绝对路径重命名：/@/xxxx => src/xxxx
-    alias: [
-      // 国际化替换
-      // {
-      //   find: 'vue-i18n',
-      //   replacement: 'vue-i18n/dist/vue-i18n.cjs.js',
-      // },
+export default ({ command, mode }: ConfigEnv): UserConfig => {
+  return {
+    resolve: {
       // 绝对路径重命名：/@/xxxx => src/xxxx
-      {
-        find: /\/@\//,
-        replacement: pathResolve('src') + '/',
-      },
-      {
-        find: /^~/,
-        replacement: '',
-      },
-    ],
-  },
-  // 服务端渲染
-  server: {
-    host: '0.0.0.0',
-    port: 8001,
-  },
-  css: {
-    preprocessorOptions: {
-      less: {
-        modifyVars: {
-          hack: `true; @import (reference) "${resolve('src/theme/index.less')}";`,
+      alias: [
+        // 国际化替换
+        // {
+        //   find: 'vue-i18n',
+        //   replacement: 'vue-i18n/dist/vue-i18n.cjs.js',
+        // },
+        // 绝对路径重命名：/@/xxxx => src/xxxx
+        {
+          find: /\/@\//,
+          replacement: pathResolve('src') + '/',
         },
-        javascriptEnabled: true,
+        {
+          find: /^~/,
+          replacement: '',
+        },
+      ],
+    },
+    // 服务端渲染
+    server: {
+      host: '0.0.0.0',
+      port: 8001,
+    },
+    css: {
+      preprocessorOptions: {
+        less: {
+          modifyVars: {
+            hack: `true; @import (reference) "${resolve('src/theme/index.less')}";`,
+          },
+          javascriptEnabled: true,
+        },
       },
     },
-  },
-  plugins: [vue()],
-})
+    plugins: [vue(),
+      viteMockServe({
+        ignore: /^\_/,
+        mockPath: 'mock',
+        localEnabled: command === 'serve',
+        prodEnabled: command !== 'serve',
+        injectCode: `
+          import { setupProdMockServer } from './mock/_createProductionServer';
+    
+          setupProdMockServer();
+          `,
+    })],
+  }
+}
