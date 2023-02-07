37bba9613cf5e09a82820d73da83750faf2d916b
liyc <38707145@qq.com>
2023-02-07 17:12:01 +0800

    修复build时编译失败的问题

--- suite-admin-web/src/main.ts
+++ suite-admin-web/src/main.ts
@@ -7,24 +7,28 @@ import router, { setupRouter } from "/@/router/index";
 import { setupStore } from "/@/store";
 import "nprogress/nprogress.css";
 
-const app = createApp(App);
+async function bootstrap() {
+  const app = createApp(App);
 
-app.use(Antd);
-app.use(router);
+  app.use(Antd);
+  app.use(router);
 
-// 注册图标组件
-const icons: any = antIcons;
-Object.keys(icons).forEach((key) => {
-  app.component(key, icons[key]);
-});
+  // 注册图标组件
+  const icons: any = antIcons;
+  Object.keys(icons).forEach((key) => {
+    app.component(key, icons[key]);
+  });
 
-// 挂载状态管理
-setupStore(app);
+  // 挂载状态管理
+  setupStore(app);
 
-// 挂载路由
-await setupRouter(app);
+  // 挂载路由
+  await setupRouter(app);
 
-// 路由准备就绪后挂载APP实例
-await router.isReady();
+  // 路由准备就绪后挂载APP实例
+  await router.isReady();
 
-app.mount("#app");
+  app.mount("#app");
+}
+
+void bootstrap();
